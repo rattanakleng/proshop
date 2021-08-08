50d63843db84286c550cea9e2ceb805c1d94ef4f
Rattanak Leng <rattanakleng@outlook.com>
2021-08-08 13:57:58 -0700

    create backendfolder and express file

---
+++ backend/data/products.js
@@ -0,0 +1,94 @@
+const products = [
+   {
+      _id: '1',
+      name: 'Camera 1',
+      image: '/images/products/product-1.jpg',
+      summary:
+         'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality',
+      description:
+         'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+   },
+   {
+      _id: '2',
+      name: 'Camera 2',
+      image: '/images/products/product-2.jpg',
+      summary:
+         'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of',
+      description:
+         'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 599.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+   },
+   {
+      _id: '3',
+      name: 'Camera 3',
+      image: '/images/products/product-3.jpg',
+      summary:
+         'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself',
+      description:
+         'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 929.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+   },
+   {
+      _id: '4',
+      name: 'Camera 4',
+      image: '/images/products/product-4.jpg',
+      summary:
+         'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming',
+      description:
+         'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 12,
+   },
+   {
+      _id: '5',
+      name: 'Camera 5',
+      image: '/images/products/product-5.jpg',
+      summary:
+         'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse.',
+      description:
+         'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      brand: 'Logitech',
+      category: 'Electronics',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+   },
+   {
+      _id: '6',
+      name: 'Camera 6',
+      image: '/images/products/product-6.jpg',
+      summary:
+         'Meet Echo Dot - Our most popular smart speaker with a fabric design.',
+      description:
+         'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      brand: 'Amazon',
+      category: 'Electronics',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 12,
+   },
+];
+
+module.export = products;
